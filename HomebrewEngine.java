27afb7ca99afbdafcc9de8d07027a514de3e8fd4
Sean Hordines <schordines@gmail.com>
2022-04-07 13:12:31 -0400

    rebuilt the base graphics engine

--- HomebrewEngine.java
+++ HomebrewEngine.java
@@ -1,27 +1,44 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class HomebrewEngine extends JPanel
+public class HomebrewEngine
 {
+    private static JFrame window = null;
+    private static final int WIDTH = 800, HEIGHT = 600;
+
     public static void main(String[] args)
     {
-        JFrame frame1 = new JFrame("Pokemon(tm): Java Edition");
-        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame1.getContentPane().add("Center", new HomebrewEngine());
-        frame1.pack();
-        frame1.setSize(new Dimension(800, 600));
-        frame1.setVisible(true);
+        makeWindow(WIDTH, HEIGHT);
+        makeBattleScene();
+        window.pack();
+        window.setVisible(true);
     }
 
-    public HomebrewEngine()
+    public static void makeWindow(int w, int h)
     {
-        setBackground(new Color(40, 40, 40));
-        System.out.println("Is it working?");
+        if(window == null)
+        {
+            window = new JFrame("Pokemon(tm): Java Edition");
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.setSize(w, h);
+            window.setResizable(false);
+            window.getContentPane().setBackground(new Color(64, 64, 64));
+            window.setLayout(new BorderLayout());
+        }
     }
 
-    public void paint(Graphics g)
+    public static void makeBattleScene()
     {
-        Graphics2D g2D = (Graphics2D) g;
-        g2D.drawImage(getToolkit().getImage("test.jpg"), null, this);
+        JPanel battleScene = new JPanel();
+        battleScene.setBackground(new Color(128, 0, 0));
+        battleScene.setPreferredSize(new Dimension(WIDTH, (int) ((float) HEIGHT * 0.8f)));
+        JLabel image = new JLabel(new ImageIcon("test.jpg"));
+        battleScene.add(image);
+        window.add(battleScene, BorderLayout.CENTER);
+
+        JPanel battleText = new JPanel();
+        battleText.setBackground(new Color(0, 128, 0));
+        battleText.setPreferredSize(new Dimension(WIDTH, (int) ((float) HEIGHT * 0.2f)));
+        window.add(battleText, BorderLayout.SOUTH);
     }
 }
